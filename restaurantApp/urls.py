dc64880e55a28baf3d6f8eb4f127b22f00954239
leifrosenkranz <rosenkranz.leif@gmail.com>
2025-02-14 09:57:02 +0100

    API Endpoints

--- restaurantApp/urls.py
+++ restaurantApp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [ 
     path('home/', views.sayHello, name='sayHello'),
     path('restaurant/', views.index, name='index'),
+    path('menu/', views.MenuItemView.as_view(), name='menu'),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='single_menu'),
 ]
--- restaurantApp/views.py
+++ restaurantApp/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .serializers import MenuSerializer,BookingSerializer
+from .models import Menu,Booking
+from rest_framework import generics,viewsets
 
 def sayHello(request):
  return HttpResponse('Hello World')
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    
+    
+    
